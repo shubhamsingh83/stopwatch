9875849d81eab3be8f081e064ad93e624181c40c
shubhamsingh83 <akashsingh832001@gmail.com>
2024-03-25 03:22:20 +0530

    stopwatch

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+import { useState, useEffect } from "react";
 import './App.css';
 
 function App() {
+  const[timer,setTimer]=useState(0);
+  const[isRunning,setIsRunning]=useState(false);
+
+  useEffect(()=>{
+    let timerId;
+    if (isRunning) {
+      timerId = setInterval(() => {
+        //  stale state(jab value update na ho ) ki vjh se yeah syntax use kr rhe  hai q ki settimer(timer+1) pe issue 
+        setTimer((prevTimer) => {
+          return prevTimer + 1;
+        });
+      }, 1000);
+    }
+    return () => {
+      clearInterval(timerId);
+    };
+  },[isRunning]);
+
+  const formatTime = (secs)=>{
+    const mins = Math.floor(secs / 60);
+    const remainingSecs = secs % 60;
+    return `${mins}:${remainingSecs < 10 ? "0" : ""}${remainingSecs}`; // 1:08 means < 0 pr 08
+  };
+
+  const startStop = () => {
+    setIsRunning(!isRunning);
+  };
+  const reset = () => {
+    setTimer(0);
+    setIsRunning(false);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <h1>Stopwatch</h1>
+       <p>Time: {formatTime(timer)}</p>
+       <button onClick={startStop}>{isRunning ? "Stop" : "Start"}</button>
+      <button onClick={reset}>Reset</button>
     </div>
   );
 }
